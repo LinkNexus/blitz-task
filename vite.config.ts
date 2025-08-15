e13e88cfa7f0a2e6d7a08aa35fae702ae3ead204
levynkeneng <deitykings@gmail.com>
2025-08-15 12:47:33 +0200

    Update `compose.override.yaml` and `vite.config.ts` to configure new dev server port and simplify Vite plugin setup
    This commit updates `compose.override.yaml` by exposing the Vite development server port (5173:5173) and simplifies `vite.config.ts` by removing redundant hostname configuration and unused strict port settings. Enhances maintainability by decluttering the Vite server settings and aligning them with default configurations.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react';
 import path from "node:path";
-import process from "node:process";
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import symfonyPlugin from "vite-plugin-symfony";
-
-const sitename = process.env.SITE_NAME || "localhost";
 
 export default defineConfig({
   plugins: [
-    symfonyPlugin({
-      viteDevServerHostname: sitename
-    }),
+    symfonyPlugin(),
     react(),
     tailwindcss()
   ],
@@ -23,12 +18,9 @@ export default defineConfig({
     }
   },
   server: {
-    strictPort: true,
-    port: 5173,
     cors: {
       origin: "*",
     },
-    allowedHosts: [sitename],
     watch: {
       ignored: [
         "**/node_modules/**",
